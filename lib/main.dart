4216795996e316bfa232fcb99aca577edad660b1
Ruthika <ruthirockstar2218@gmail.com>
2024-03-30 07:15:02 +0530

    structure

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TicTacToe());
+}
+
+class TicTacToe extends StatefulWidget {
+  const TicTacToe({super.key});
+
+  @override
+  State<TicTacToe> createState() => _TicTacToeState();
+}
+
+class _TicTacToeState extends State<TicTacToe> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/game.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  // static var customWhite = GoogleFonts.coiny{
+
+  // }
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
